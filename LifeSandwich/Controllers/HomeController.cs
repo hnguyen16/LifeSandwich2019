ac0c0ce21dd2b30795692f0cea73475335959d5f
hnguyen16 <bobby_n475@yahoo.com>
2019-01-31 21:35:46 -0600

    test

---
+++ LifeSandwich/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using LifeSandwich.Models;
+using LifeSandwich.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LifeSandwich.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            string[] files = System.IO.Directory.GetFiles(Server.MapPath("~/content/Images"));
+
+            IndexViewModel indexVM = new IndexViewModel();
+
+            foreach (string f in files)
+            {
+                Image pic = new Image();
+                List<string>  s = f.Split('/').ToList();
+                pic.name = "~/Content/Images/" + s.Last();
+                indexVM.images.Add(pic);
+            }
+            return View(indexVM);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ LifeSandwich/ViewModels/IndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿using LifeSandwich.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LifeSandwich.ViewModels
+{
+    public class IndexViewModel
+    {
+        public List<Image> images = new List<Image>();
+    }
+}
